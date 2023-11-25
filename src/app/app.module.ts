d0eafc1a0136f59c41682df1a5a23491f7a72859
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-11-25 15:21:11 +0600

    update work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, DashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'student-management-syste-78117',
+        appId: '1:487154858379:web:03d9bc2ed624e06152178e',
+        storageBucket: 'student-management-syste-78117.appspot.com',
+        apiKey: 'AIzaSyAyEiHGuwkLP-gVcdX9tDUTcJjVcm76hCY',
+        authDomain: 'student-management-syste-78117.firebaseapp.com',
+        messagingSenderId: '487154858379',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyAyEiHGuwkLP-gVcdX9tDUTcJjVcm76hCY',
+    authDomain: 'student-management-syste-78117.firebaseapp.com',
+    projectId: 'student-management-syste-78117',
+    storageBucket: 'student-management-syste-78117.appspot.com',
+    messagingSenderId: '487154858379',
+    appId: '1:487154858379:web:03d9bc2ed624e06152178e',
+  },
+};
